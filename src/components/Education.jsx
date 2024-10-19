25b2da106cba52fba88b89059911a5f2a3e136c1
Tareq <razzbabut@gmail.com>
2024-10-20 04:08:37 +0600

    added animation in education component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 
 const Education = () => {
     return (
@@ -6,29 +7,46 @@ const Education = () => {
             className="w-full md:h-screen p-4 text-white pt-20"
         >
             <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-                <div className='mb-12'>
+                <motion.div
+                    whileInView={{ opacity: 1, y: 0 }}
+                    initial={{ opacity: 0, y: -100 }}
+                    transition={{ duration: 1.5 }}
+                    className='mb-12'>
                     <p className="text-4xl font-bold border-b-4 border-gray-500 py-2 inline">
                         Education
                     </p>
                     <p className="py-4">Checkout of my educational background right here</p>
-                </div>
-                <div className='mb-[20px] text-white bg-gray-700/20 p-4 rounded-3xl w-full text-center md:max-w-[600px] mx-auto'>
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+
+                    className='mb-[20px] text-white bg-gray-700/20 p-4 rounded-3xl w-full text-center md:max-w-[600px] mx-auto'>
                     <p>Daffodil International University</p>
                     <p>Bachelor of Science in CSE</p>
                     <p>(2016-2020)</p>
-                </div>
-                <div className='h-12 w-0.5 bg-slate-400 my-1 mx-auto'></div>
-                <div className='mb-[20px] text-white bg-gray-700/20 p-4 rounded-3xl w-full text-center md:max-w-[600px] mx-auto'>
+                </motion.div>
+                <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -30 }} transition={{ duration: 1 }} className='h-12 w-0.5 bg-slate-400 my-1 mx-auto'></motion.div>
+                <motion.div 
+                whileInView={{opacity:1, x:0}}
+                initial={{opacity:0, x:100}}
+                transition={{duration:1}}
+                className='mb-[20px] text-white bg-gray-700/20 p-4 rounded-3xl w-full text-center md:max-w-[600px] mx-auto'>
                     <p>Uttara Town College</p>
                     <p>Higher Secondary School Certificate</p>
                     <p>(2013-2014)</p>
-                </div>
-                <div className='h-12 w-0.5 bg-slate-400 my-1 mx-auto'></div>
-                <div className=' text-white bg-gray-700/20 p-4 rounded-3xl w-full text-center md:max-w-[600px] mx-auto'>
+                </motion.div>
+                <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -30 }} transition={{ duration: 1 }} className='h-12 w-0.5 bg-slate-400 my-1 mx-auto'></motion.div>
+                <motion.div 
+                whileInView={{opacity:1, x:0}}
+                initial={{opacity:0, x:-100}}
+                transition={{duration:1}}
+                className=' text-white bg-gray-700/20 p-4 rounded-3xl w-full text-center md:max-w-[600px] mx-auto'>
                     <p>Mahmudpur High School</p>
                     <p>Secondary School Certificate</p>
                     <p>(2011-2012)</p>
-                </div>
+                </motion.div>
             </div>
         </div>
     );
