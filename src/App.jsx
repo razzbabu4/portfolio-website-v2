ca8982114be8bec28f70f82a86534b832834384f
Tareq <razzbabut@gmail.com>
2024-10-20 07:21:38 +0600

    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
 
       {/* Content start from here */}
       <Navbar/>
-      <Hero />
-      <Skills/>
+      <Hero/>
+      <Skills />
       <Projects/>
       <Education/>
       <Contact/>
