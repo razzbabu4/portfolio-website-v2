895a9025efcb1310d1e0ca265b71613d23b60b6b
Tareq <razzbabut@gmail.com>
2024-10-20 06:10:16 +0600

    fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Contact from "./components/Contact"
 import Education from "./components/Education"
 import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 
